3a39419b709f6faa2fb4a32cb75297d503d43526
Tadeáš Fořt <business@tadeasfort.com>
2024-10-05 08:53:37 +0200

    Delete swagger-autogen.js

--- swagger-autogen.js
+++
@@ -1,6 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')();
-
-const outputFile = './swagger-fb-messenger-viewer-express.json';
-const endpointsFiles = ['./index.js'];
-
-swaggerAutogen(outputFile, endpointsFiles);
